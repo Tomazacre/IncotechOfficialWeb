495524b47b9e546f4485d69abe6422699a65c7ad
tomazacre <61418586+Tomazacre@users.noreply.github.com>
2026-02-23 18:11:06 +0000

    CommitV1.3

--- incotech-app/src/Componentes/COM_navBar.tsx
+++ incotech-app/src/Componentes/COM_navBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+//import React from "react";
+import { useState, useEffect } from "react";
 import logo from "../assets/IncotechLogo.png";
 
 // Componentes y hooks de Material UI
--- incotech-app/src/main.tsx
+++ incotech-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+//import App from "./App.tsx";
 //import PruebaHD from "./pruebasEnCodigo.tsx";
 //import DottedConnector from "./pruebasEnCodigo.tsx";
 //import Navbar from "./Componentes/COM_navBar.tsx";
